c938f526a588158dcce3bc4501cae356d931212f
urshivamthakur <shivamthakur0620@gamil.com>
2025-01-05 16:00:26 +0530

    Comments

--- Variables & Data Types/JavaBasics.java
+++ Variables & Data Types/JavaBasics.java
@@ -1,8 +1,15 @@
 public class JavaBasics {
     public static void main(String[] args) {
+        // code to calculate sum
         int a = 10;
         int b = 10;
         int sum = a + b;
         System.out.println(sum);
+        /*
+         Hi, Shivam
+         wow
+         hello
+         bye
+         */
     }
 }
