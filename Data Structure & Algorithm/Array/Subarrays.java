d1d3e40b438a3432338ced9a52a03bd40678b2e8
urshivamthakur <shivamthakur0620@gamil.com>
2025-01-25 02:03:47 +0530

    Calculated sum of subArrays problem

--- Data Structure & Algorithm/Array/Subarrays.java
+++ Data Structure & Algorithm/Array/Subarrays.java
@@ -1,19 +1,31 @@
 public class Subarrays {
     public static void printSubarrays(int arr[]) {
-        int ts = 0;
+        int totalSum = 0; // Variable to store the sum of all subarrays
+        int ts = 0; // Variable to count the total number of subarrays
+
         for (int i = 0; i <= arr.length - 1; i++) {
             int start = i;
             for (int j = i; j < arr.length; j++) {
                 int end = j;
+
+                // Calculate the sum of the current subarray
+                int subarraySum = 0;
                 for (int k = start; k <= end; k++) { // print
+                    subarraySum += arr[k];
                     System.out.print(arr[k] + " "); // subarrays
                 }
+                // Add the subarray sum to the total sum
+                totalSum += subarraySum;
+
+                // Increment total subarray count
                 ts++;
-                System.out.println();
+
+                System.out.println("=> Subarray sum: " + subarraySum);
             }
             System.out.println();
         }
-        System.out.println(ts);
+        System.out.println("Total number of subarrays: " + ts);
+        System.out.println("Sum of all subarrays " + totalSum);
     }
 
     public static void main(String[] args) {
