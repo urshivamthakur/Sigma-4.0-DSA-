80cfb7ed8c6528a80b5a851316033da69dec5c98
urshivamthakur <shivamthakur0620@gamil.com>
2025-02-10 11:25:56 +0530

    OOPS one word

---
+++ Data Structure & Algorithm/OOPS/Class_Practice_Question/Pq1.java
@@ -0,0 +1,12 @@
+public class Pq1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Shivam";
+        System.out.println(s.name);
+    }
+}
+
+class Student {
+    String name;
+    // int roll;
+}
